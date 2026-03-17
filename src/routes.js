df6701cbb149c8dff2b126c4faa7defacbffaf79
Tharcio Santos <tharcio09@hotmail.com>
2026-03-17 05:14:59 -0300

    feat(routes): centraliza os endpoints da API
    Cria gerenciador de rotas com o Express Router e mapeia as requisições para o userController.

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { rotaInicial, pegarUsuarios, cadastrarUsuario, deletarUsuario, editarUsuario } from './controllers/userController.js';
+
+export const routes = Router();
+
+routes.get('/', rotaInicial);
+routes.get('/usuario', pegarUsuarios);
+routes.post('/usuario', cadastrarUsuario);
+routes.delete('/usuario/:id', deletarUsuario);
+routes.put('/usuario/:id', editarUsuario);
