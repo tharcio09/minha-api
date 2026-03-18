8e237b71649898c6a844f087f734576976c85324
Tharcio Santos <tharcio09@hotmail.com>
2026-03-18 16:24:13 -0300

    feat(auth): adiciona criptografia de senha no model de usuário
    Implementa hook pre('save') do Mongoose com a biblioteca bcrypt para realizar o hash automático da senha antes da persistênca no banco de dados.

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,20 @@
+import bcrypt from 'bcrypt';
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: { type: String },
     email: { type: String },
     password: { type: String }
+});
+
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) {
+        return;
+    }
+
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+
 });
 
 export const User = mongoose.model("User", userSchema);
