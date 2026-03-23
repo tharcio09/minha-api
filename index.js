89dcf716c933b1ea49c5c8693ed18057e30ae371
Tharcio Santos <tharcio09@hotmail.com>
2026-03-23 06:31:01 -0300

    chore(config): adiciona middleware CORS para permitir requisições do front-end

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './src/routes.js';
 import { mongo } from './src/db.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 mongo();
