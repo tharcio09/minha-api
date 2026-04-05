6373ad765ac2fc896c16e7e55a3731bd517f4a36
Tharcio Santos <tharcio09@hotmail.com>
2026-04-05 13:21:54 -0300

    fix(cors): restringe origens permitidas e remove barra final da url de producao

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://frontend-api-weld.vercel.app/'
+        'https://frontend-api-weld.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
